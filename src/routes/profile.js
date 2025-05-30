94e0ea2c673f4a4bb503028448645de05c3d1117
NagarajuTirumani <108359226+NagarajuTirumani@users.noreply.github.com>
2025-05-30 16:55:47 +0530

    Profile edit api implementation

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,15 +1,32 @@
 const express = require("express");
 
 const { authUser } = require("../middleware/auth");
+const { validateEditUserData } = require("../utils/validation");
+const UserModel = require('../models/users');
 
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", authUser, async (req, res) => {
+profileRouter.get("/profile/view", authUser, async (req, res) => {
   try {
     const { user } = req.body;
     res.send(user);
   } catch (error) {
     res.status(500).send(error.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", authUser, async (req, res) => {
+  try {
+    validateEditUserData(req);
+    const { user, ...restData } = req.body;
+    Object.keys(restData).forEach((key) => user[key] = req.body[key]);
+    await user.save();
+    res.json({
+      message: 'User Updated',
+      data: user,
+    })
+  } catch (error) {
+    res.status(500).send("Fail to Update: " + error.message);
   }
 });
 
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -7,6 +7,29 @@ const validateSignUpData = (data) => {
   }
 };
 
+const validateEditUserData = (req) => {
+  const { user, ...restData } = req.body;
+  const allowedFeilds = [
+    "firstName",
+    "lastName",
+    "age",
+    "gender",
+    "skills",
+    "about",
+  ];
+  const isAllowedDataToUpdate = Object.keys(restData).every((key) =>
+    allowedFeilds.includes(key)
+  );
+  if (!isAllowedDataToUpdate) {
+    throw new Error(
+      "Some of these feilds are not allowed to update. Please check again"
+    );
+  } else if (restData?.skills?.length > 10) {
+    throw new Error("Max length for skill set is 10.");
+  }
+};
+
 module.exports = {
   validateSignUpData,
+  validateEditUserData,
 };
